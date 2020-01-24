a854ceb5f81679772ee92b121d0a60ff7e821ef6
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-01-24 06:32:50 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,19 +1,16 @@
 def my_hash
- your_hash = {
- "pineapple => "best fruit",
+  your_hash = {
+  "pineapple => "best fruit",
   "peanuts" => "I am not allergic",
   "Mickey Mouse" => "favorite character so far"
   }
-end
-
-
+  end
+  
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 the_manifest = {
-"wja;e"
-  "
+"whale bone corsets" => 5
+  "porcelain vases" => 2 
+  "oil paintings" => 3
 }
 
 end
